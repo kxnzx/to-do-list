9eae473fd7310a1d4bde46ddff5eea86fff8e5bb
“kxnzx” <k.bousboa@gmail.com>
2024-04-12 13:09:21 +0200

    change app component title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,15 @@ describe('AppComponent', () => {
   it(`should have the 'to-do-list' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('to-do-list');
+    expect(app.componentTitle).toEqual('to-do-list');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, to-do-list');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, to-do-list'
+    );
   });
 });
